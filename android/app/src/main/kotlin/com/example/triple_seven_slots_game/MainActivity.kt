9f4f63ac5a76bdc62151b842d8fe0f98cfa44528
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-18 14:28:39 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/triple_seven_slots_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.triple_seven_slots_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:triple_seven_slots_game/app.dart';
+
+void main() {
+  runApp(const App());
+}
