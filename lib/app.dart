2c0792df76ffb5faf4a3a44c28610cee35632208
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-19 13:49:11 +0200

    Add entrypoint to app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:triple_seven_slots_game/routes.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Triple Seven Slots Game',
+      routes: routes,
+      initialRoute: mainMenuScreen,
+    );
   }
 }
