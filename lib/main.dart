d8edcaa0d5e4082b41a9ced998273887d266c20e
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-19 15:58:22 +0200

    Change device orientation to ONLY horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:triple_seven_slots_game/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
   runApp(const App());
 }
