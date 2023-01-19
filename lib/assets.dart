1714e46659fac3d9924065da25a6d0b6a43c24fc
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-19 13:48:57 +0200

    Add assets

---
+++ lib/assets.dart
@@ -0,0 +1,9 @@
+/// Slot items
+const seventhIc = 'assets/images/777.svg';
+const cherryIc = 'assets/images/cherry.svg';
+const appleIc = 'assets/images/apple.svg';
+const barIc = 'assets/images/bar.svg';
+const coinIc = 'assets/images/coin.svg';
+const crownIc = 'assets/images/crown.svg';
+const lemonIc = 'assets/images/lemon.svg';
+const watermelonIc = 'assets/images/watermelon.svg';
