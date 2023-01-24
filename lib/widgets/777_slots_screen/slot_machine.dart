122b4c81648c2ea3202c8cd589b7bf40ad697893
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-24 12:42:00 +0200

    Add controllers, control panel to slot_machine.dart,

--- lib/widgets/777_slots_screen/slot_machine.dart
+++ lib/widgets/777_slots_screen/slot_machine.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:roll_slot_machine/roll_slot.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart' as svg_provider;
+import 'package:roll_slot_machine/roll_slot_controller.dart';
 import 'package:triple_seven_slots_game/assets.dart';
-
-const boardWidth = 430.0;
+import 'package:triple_seven_slots_game/widgets/777_slots_screen/common_roll_slot.dart';
+import 'package:triple_seven_slots_game/widgets/777_slots_screen/control_panel.dart';
 
 class SlotMachine extends StatefulWidget {
   const SlotMachine({Key? key}) : super(key: key);
@@ -13,6 +15,11 @@ class SlotMachine extends StatefulWidget {
 }
 
 class _SlotMachineState extends State<SlotMachine> {
+  final RollSlotController _firstController = RollSlotController();
+  final RollSlotController _secondController = RollSlotController();
+  final RollSlotController _thirdController = RollSlotController();
+  final RollSlotController _fourthController = RollSlotController();
+
   final List<Widget> prizes = <Widget>[
     Center(child: SvgPicture.asset(seventhIc)),
     Center(child: SvgPicture.asset(cherryIc)),
@@ -26,57 +33,64 @@ class _SlotMachineState extends State<SlotMachine> {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Stack(
-        children: [
-          SvgPicture.asset(
-            boardIc,
-            width: boardWidth,
-            height: 330,
-          ),
-          Positioned(
-            top: 100,
+    final size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Flexible(
+          child: AbsorbPointer(
             child: Container(
-              constraints: const BoxConstraints(maxWidth: boardWidth - 40, maxHeight: 180),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 60),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                      child: RollSlot(
-                        squeeze: 1.2,
-                        diameterRation: 10.5,
-                        itemPadding: EdgeInsets.zero,
-                        itemExtend: 60,
-                        children: prizes,
-                      ),
-                    ),
-                    Flexible(
-                      child: RollSlot(
-                        squeeze: 0.95,
-                        diameterRation: 5.5,
-                        itemPadding: EdgeInsets.zero,
-                        itemExtend: 70,
-                        children: prizes,
-                      ),
-                    ),
-                    Flexible(
-                      child: RollSlot(
-                        squeeze: 1.0,
-                        diameterRation: 2.5,
-                        itemPadding: EdgeInsets.zero,
-                        itemExtend: 70,
-                        children: prizes,
-                      ),
-                    ),
-                  ],
+              width: size.width * 0.45,
+              height: size.height * 0.7,
+              padding: EdgeInsets.only(
+                  top: size.height * 0.2, bottom: size.height * 0.1, left: 50, right: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: svg_provider.Svg(boardIc),
+                  fit: BoxFit.cover,
                 ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: _buildRollSlots(),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+        const ControlPanel(),
+      ],
     );
+  }
+
+  List<Widget> _buildRollSlots() {
+    return [
+      Flexible(
+        child: CommonRollSlot(
+          controller: _firstController,
+          itemExtend: 50,
+          prizes: prizes,
+        ),
+      ),
+      Flexible(
+        child: CommonRollSlot(
+          controller: _secondController,
+          itemExtend: 50,
+          prizes: prizes,
+        ),
+      ),
+      Flexible(
+        child: CommonRollSlot(
+          controller: _thirdController,
+          itemExtend: 50,
+          prizes: prizes,
+        ),
+      ),
+      Flexible(
+        child: CommonRollSlot(
+          controller: _fourthController,
+          itemExtend: 50,
+          prizes: prizes,
+        ),
+      ),
+    ];
   }
 }
