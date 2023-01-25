0ec6d43edefc89e1913b5db5683292abeb5e0e60
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-26 00:34:32 +0200

    Create fortune_item.dart

---
+++ lib/widgets/spin_wheel/fortune_item.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:triple_seven_slots_game/assets.dart';
+import 'package:triple_seven_slots_game/theme.dart';
+
+class CustomFortuneItem extends StatelessWidget {
+  final int multiplier;
+
+  const CustomFortuneItem({required this.multiplier, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      width: 250,
+      decoration: const BoxDecoration(
+        gradient: orangeGradientLight,
+      ),
+      child: Transform.rotate(
+        angle: pi / 2,
+        child: SizedBox(
+          height: 90,
+          width: 90,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Center(child: SvgPicture.asset(coinIc, height: 50)),
+                  Positioned.fill(
+                    top: 20,
+                    left: 10,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(star),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'x$multiplier',
+                            style: TextStyles.bodyReg16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
