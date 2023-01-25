9b87dfa3120987df829261ea980b76dd378975c6
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-25 17:48:25 +0200

    Add assets for spin wheel

---
+++ lib/screens/spin_wheel_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpinWheelScreen extends StatelessWidget {
+  const SpinWheelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
