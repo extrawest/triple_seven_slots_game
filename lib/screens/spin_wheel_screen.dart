b10f673f1a285ea6e345871db8cce839d78b478f
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-01-26 17:13:36 +0200

    Refactor spin_wheel_screen.dart

--- lib/screens/spin_wheel_screen.dart
+++ lib/screens/spin_wheel_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:triple_seven_slots_game/bloc/spin_wheel_cubit/spin_wheel_cubit.dart';
 import 'package:triple_seven_slots_game/bloc/spin_wheel_cubit/spin_wheel_state.dart';
+import 'package:triple_seven_slots_game/theme.dart';
 import 'package:triple_seven_slots_game/widgets/common/background_gradient_scaffold.dart';
 import 'package:triple_seven_slots_game/widgets/spin_wheel/spin_button.dart';
 import 'package:triple_seven_slots_game/widgets/spin_wheel/spin_wheel.dart';
@@ -20,17 +21,23 @@ class SpinWheelScreen extends StatelessWidget {
       create: (context) => _fortuneWheelNotifier,
       builder: (context, child) => GradientBackgroundScaffold(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Flexible(child: SpinWheel()),
+            const Flexible(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: SpinWheel(),
+              ),
+            ),
+            const SizedBox(width: 20),
             Flexible(
               child: BlocBuilder<SpinWheelCubit, SpinWheelState>(
-                builder: (context, state) => Column(
-                  children: [
-                    if (!state.isWheelAvailable) Text(state.timeLeft),
-                    const SpinButton(),
-                  ],
-                ),
+                builder: (context, state) => state.isWheelAvailable
+                    ? const SpinButton()
+                    : Text(
+                        state.timeLeft,
+                        style: TextStyles.clarendonReg22,
+                      ),
               ),
             ),
           ],
